5f2ec0ab9bb26f246e47a304baa3255b214925a9
TomasEdH <tomasestrada1209@gmail.com>
2024-03-14 23:24:26 -0500

    last modifications to portfolio project

---
+++ index.js
@@ -0,0 +1,19 @@
+/*const names = ['midu', 'ana', 'pedro']
+
+const found = names.sort().find(n => n.includes('a'))
+
+console.log(names[0] === found)
+
+let numeroConsola = 0
+let limiteConsola = 10
+
+for(let i = 0; i < limiteConsola; i++){
+    numeroConsola += 1
+    console.log(`Hola mundo ${numeroConsola}`)
+}*/
+
+let palabra = 'usuario%contraseña'
+const palabraSpliteada = palabra.split('%')
+console.log(palabraSpliteada)
+
+
